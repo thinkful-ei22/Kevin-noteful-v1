6949046195251c518204d945071e7aaed27ba4d4
Kevin <ketsang12@gmail.com>
2018-06-25 15:07:21 -0400

    broken code

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const express = require('express');
 const app = express();
 
 // ADD STATIC SERVER HERE
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
