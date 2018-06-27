26255c0dabf33447afe0da013f2cac4b2d7c9f73
Kevin <ketsang12@gmail.com>
2018-06-27 15:44:18 -0400

    new router file. modularize

---
+++ router/notes.router.js
@@ -0,0 +1,102 @@
+'use strict';
+const express = require('express');
+const routerNotes = express.Router();
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+routerNotes.get('/', (req, res, next) => {
+  console.log('i sent a get request');
+  const {searchTerm} = req.query;
+  // if(searchTerm){
+  //   const newData = data.filter (item => item.title.includes(searchTerm));
+  //   res.json(newData);
+  // } else {
+  //   res.json(data);
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+  
+// app.get('/api/notes/:id', (req, res) => {
+//   const {id} = req.params;
+//   const dataNew = data.find(item => item.id ===Number(id));
+//   return res.json(dataNew);
+// });
+  
+routerNotes.get('/:id', (req, res, next) => {
+  console.log('id ran');
+  const {id} = req.params;
+  // notes.find(id, (err, item) =>{
+  //   if(err){
+  //     return res.status(404).next(err);
+  //   }
+  notes.find(id, (err, item) => {
+    if (err) {
+      console.log('error', err);
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    }else {
+      console.log('item not found');
+      next();
+    }
+  });
+});
+  
+routerNotes.put('/:id', (req, res, next) => {
+  console.log('put ran');
+  const {id} = req.params;
+  
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+      console.log(updateObj);
+    }
+  });
+  
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+// Post (insert) an item
+routerNotes.post('/', (req, res, next) => {
+  const { title, content } = req.body;
+  
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+module.exports = {routerNotes};
+  
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 'use strict';
-/* globals next*/
 // Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB');  // <<== add this
-const notes = simDB.initialize(data); // <<== and this
+
 // console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
@@ -15,77 +12,12 @@ app.use(express.static('public'));
 app.use(express.json());
 const { PORT } = require('./config');
 const morgan = require('morgan');
-
 app.use(morgan('dev'));
 
-app.get('/api/notes', (req, res) => {
-  console.log('i sent a get request');
-  const {searchTerm} = req.query;
-  // if(searchTerm){
-  //   const newData = data.filter (item => item.title.includes(searchTerm));
-  //   res.json(newData);
-  // } else {
-  //   res.json(data);
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
+const {routerNotes} = require('./router/notes.router');
 
-// app.get('/api/notes/:id', (req, res) => {
-//   const {id} = req.params;
-//   const dataNew = data.find(item => item.id ===Number(id));
-//   return res.json(dataNew);
-// });
+app.use('/api/notes', routerNotes);
 
-app.get('/api/notes/:id', (req, res, next) => {
-  console.log('id ran');
-  const {id} = req.params;
-  // notes.find(id, (err, item) =>{
-  //   if(err){
-  //     return res.status(404).next(err);
-  //   }
-  notes.find(id, (err, item) => {
-    if (err) {
-      console.log('error', err);
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    }else {
-      console.log('item not found');
-      next();
-    }
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  console.log('put ran');
-  const {id} = req.params;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-      console.log(updateObj);
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
 
 // app.get('/boom', (req, res, next) => {
 //   throw new Error('Boom!!');
